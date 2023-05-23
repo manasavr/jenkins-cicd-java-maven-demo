4773be5028a40a7751496726745c56ab66582abc
manasavr <128247550+manasavr@users.noreply.github.com>
2023-05-23 11:58:57 +0530

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -16,7 +16,7 @@ public class Hello extends HttpServlet {
    public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
-      // Set response content type
+     
       response.setContentType("text/html");
 
       // Actual logic goes here.
